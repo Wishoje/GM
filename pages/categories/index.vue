abf4213c968637e1ce413acbfb53c3059ed02770
Wishoje <kovacevic.milan88@gmail.com>
2020-05-11 17:14:40 -0700

    add categories page

---
+++ pages/categories/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<main>
+		<categories :isCategoryPage="true" :limit="20" />
+	</main>
+</template>
+
+<script>
+
+import categories from '../../components/categories';
+
+export default {
+	name: 'homePage',
+	components: {
+		categories
+	}
+}
+</script>
+
+<style>
+</style>
