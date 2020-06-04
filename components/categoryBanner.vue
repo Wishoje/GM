0b0d2f87b22c8074a54593ac16287748ea5739e0
Igor Todorovic <igor333m@yahoo.com>
2020-06-03 21:52:28 -0700

    Add search filter component for genre

--- components/categoryBanner.vue
+++ components/categoryBanner.vue
@@ -1,10 +1,10 @@
 <template>
-	<article class="c-banner">
+	<article class="m-banner">
 		<h2>Explore Our Musical World</h2>
 		<h3>Find your favorite playlist</h3>
 
-		<div class="c-search-box">
-            <input class="c-search-text" placeholder='Search for game, platform or genre' onblur="this.placeholder='Search for game, platform or genre'" onfocus="this.placeholder=''">
+		<div class="m-search-box">
+            <input class="m-search-text" placeholder='Search for game, platform or genre' onblur="this.placeholder='Search for game, platform or genre'" onfocus="this.placeholder=''">
 		</div>
 	</article>
 </template>
@@ -17,36 +17,4 @@
 </script>
 
 <style lang="scss" scoped>
-	.c-banner {
-		padding: 90px 0;
-		background-image: url('/images/banner.jpg');
-		color: $primary-white;
-		text-align: center;
-    }
-    
-    .c-search-box {
-        width: 40%;
-        margin: 0 auto;
-    }
-
-    .c-search-text {
-        background:white;
-        border-radius: 0.2rem;
-        color:black;
-        padding: 1rem 0 1rem 2rem;
-		width:70%;
-	}
-
-	input::placeholder {
-		text-align: center;
-	}
-
-	input:focus {
-		color: #ff0000;
-		padding-left: 10rem;
-		@media #{$mq-desktop-large} { 
-			padding-left: 15rem;
-		}
-	}
-	
 </style>
