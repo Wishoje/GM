bd2ad1509a885c42509d396e69f6f776f5420c1c
Marko Robovic <33806997+oceancapricorn@users.noreply.github.com>
2020-06-16 11:35:29 +0200

    footer-implemetation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,31 @@
 <template>
-	<div>
-		<AppModal />
-		<navigationMobile v-if="isMobile" />
-		<navigation v-else />
-		<nuxt />
-	</div>
+  <div>
+    <AppModal />
+    <navigationMobile v-if="isMobile" />
+    <navigation v-else />
+    <nuxt />
+    <footer />
+  </div>
 </template>
 
 <script>
-	import AppModal from '../components/AppModal';
-	import navigation from '../components/navigation';
-	import navigationMobile from '../components/navigationMobile';
+import AppModal from "../components/AppModal";
+import navigation from "../components/navigation";
+import navigationMobile from "../components/navigationMobile";
+import footer from "../components/footer";
 
-	export default {
-		components: {
-			AppModal,
-			navigationMobile,
-			navigation
-		},
-		computed: {
-			isMobile() {
-				return this.$device.isMobile;
-			}
-		}
-	}
+export default {
+  components: {
+    AppModal,
+    navigationMobile,
+    navigation,
+    footer
+  },
+  computed: {
+    isMobile() {
+      return this.$device.isMobile;
+    }
+  }
+};
 </script>
 
