a5a0ce1566ef4db17d4873e011101e7abd6dfba6
Wishoje <kovacevic.milan88@gmail.com>
2020-06-25 22:55:57 -0700

    test

--- store/index.js
+++ store/index.js
@@ -6,20 +6,19 @@ export const state = () => ({})
 export const mutations = {}
 
 export const actions = {
-	async nuxtServerInit ({dispatch, commit}, context) {
+	nuxtServerInit ({dispatch, commit}, context) {
 		return new Promise((resolve, reject) => {
 			const cookies = cookie.parse(context.req.headers.cookie || '');
 			commit('auth/SET_DEFAULT_COOKIE');
 			if (cookies.hasOwnProperty('x-access-token')) {
-				try {
-					setAuthToken(cookies['x-access-token']);
-					await dispatch('auth/fetch');
+				setAuthToken(cookies['x-access-token']);
+				dispatch('auth/fetch').then(result => {
 					resolve(true);
-				} catch(err) {
-					console.log('Provided token is invalid:', err)  ;                    
+				}).catch(error => {
+					console.log('Provided token is invalid:', error)  ;                    
 					resetAuthToken();
 					resolve(false);
-				}
+				})
 			} else {
 				resetAuthToken();
 				resolve(false);
