f0536a0577f676cd238230de1857b650dfb26f84
Wishoje <kovacevic.milan88@gmail.com>
2020-06-30 23:34:03 -0700

    test

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -1,11 +1,11 @@
 import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
-	host: 'ec2-18-214-211-47.compute-1.amazonaws.com',
-	database: 'd8djh3l2mro22a',
-	username: 'gycdkioujcqecw',
+	host: 'ec2-34-192-173-173.compute-1.amazonaws.com',
+	database: 'da790omsnum82d',
+	username: 'cnbvouylkxgbph',
 	port: 5432,
-	password: '6dd499e2a5ddab249d0c9134e4bced8cca0c66d4da103b2452689ce74f8c05cd',
+	password: '22b11a2896de1f5283d9f91c4603c10c2652fc52dbe85d32c420e8bbd7052ccd',
 	url: process.env.DATABASE_URL,
 	type: 'postgres',
 	synchronize: true,
