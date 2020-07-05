e8f9c8e9130220d915392390178a70cd821b9f3d
Wishoje <kovacevic.milan88@gmail.com>
2020-07-04 19:40:53 -0700

    test

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -1,14 +1,9 @@
 import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
+	url: process.env.DATABASE_URL,
 	type: 'postgres',
-	host: 'localhost',
-	port: 5432,
-	username: 'admin',
-	password: 'admin',
-	database: 'GM',
 	synchronize: true,
-	logging: true,
 	entities: [
 		__dirname + '/../**/*.entity{.ts,.js}',
 	],
