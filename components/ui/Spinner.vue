34359659712c663c3fc78fb2ed30044c32df7a4f
Wishoje <kovacevic.milan88@gmail.com>
2020-07-24 00:21:59 -0700

    add styles to spinner

--- components/ui/Spinner.vue
+++ components/ui/Spinner.vue
@@ -36,3 +36,40 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+.vld-overlay {
+	bottom: 0;
+	left: 0;
+	position: absolute;
+	right: 0;
+	top: 0;
+	align-items: center;
+	display: none;
+	justify-content: center;
+	overflow: hidden;
+	z-index: 9999;
+}
+
+.vld-overlay.is-active {
+	display: flex;
+}
+
+.vld-overlay.is-full-page {
+	z-index: 9999;
+	position: fixed;
+}
+
+.vld-overlay .vld-background {
+	bottom: 0;
+	left: 0;
+	position: absolute;
+	right: 0;
+	top: 0;
+	opacity: 0.5;
+}
+
+.vld-overlay .vld-icon, .vld-parent {
+	position: relative;
+}
+</style>
